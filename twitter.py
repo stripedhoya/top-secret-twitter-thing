9c1c40f09dca97125f098c55b8c278e1905678e7
Rowland DePree <radepree@my.harrisburgu.edu>
2016-11-05 15:42:07 -0400

    Modify to make the redis connection and logic for redis work better.

--- twitter.py
+++ twitter.py
@@ -4,16 +4,17 @@ import os
 import time
 from datetime import datetime
 
+import pytz
 import redis
 import tweepy
 
 
 class Twitter:
     def __init__(self):
-        self.token = self.auth()
         self.config = ConfigParser.ConfigParser()
         self.config.read(os.path.abspath('config.ini'))
         self.r = redis.StrictRedis(host=self.config.get('DB', 'host'), port=self.config.get('DB', 'port'))
+        self.token = self.auth()
 
     def auth(self):
         """
@@ -35,17 +36,18 @@ class Twitter:
         :return: boolean
         """
         token = tweepy.API(self.token)
-        time_format = '%a %b %d %H:%M:%S &z %Y'
         count = 0
         results = token.search(q, geocode=geocode, count=100, result_type='recent')
-        for status in results['statuses']:
-            utc_time = datetime.strptime(status['created_at'], time_format)
-            tweet_epoch = (utc_time - datetime(1970, 1, 1)).total_seconds()
+        for status in results:
+            utc_time = datetime.strptime(str(status._json['created_at']), '%a %b %d %H:%M:%S +0000 %Y').replace(
+                tzinfo=pytz.UTC)
+            tweet_epoch = (utc_time - datetime(1970, 1, 1).replace(tzinfo=pytz.UTC)).total_seconds()
             if tweet_epoch <= epoch_time:
                 count += 1
 
         if count > 4:
-            self.insert_redis('%s: WiFi is DOWN' % geocode)
+            self.insert_redis('%s: WiFi is DOWN' % str(geocode))
+            print('Inserted into redis.')
 
     def backup_search(self, q, geocode, epoch_time):
         """
@@ -56,17 +58,18 @@ class Twitter:
         :return: boolean
         """
         token = tweepy.API(self.token)
-        time_format = '%a %b %d %H:%M:%S &z %Y'
         count = 0
         results = token.search(q, geocode=geocode, count=100, result_type='recent')
-        for status in results['statuses']:
-            utc_time = datetime.strptime(status['created_at'], time_format)
-            tweet_epoch = (utc_time - datetime(1970, 1, 1)).total_seconds()
+        for status in results:
+            utc_time = datetime.strptime(str(status._json['created_at']), '%a %b %d %H:%M:%S +0000 %Y').replace(
+                tzinfo=pytz.UTC)
+            tweet_epoch = (utc_time - datetime(1970, 1, 1).replace(tzinfo=pytz.UTC)).total_seconds()
             if tweet_epoch <= epoch_time:
                 count += 1
 
         if count > 4:
-            self.insert_redis('%s: WiFi is UP' % geocode)
+            self.insert_redis('%s: WiFi is UP' % str(geocode))
+            print('Inserted into redis.')
 
     def insert_redis(self, value):
         """
@@ -75,3 +78,8 @@ class Twitter:
         :return:
         """
         self.r.set(time.time(), value)
+
+
+if __name__ == '__main__':
+    twt = Twitter()
+    twt.down_search('wifi down', None, time.time())
